1aa6e028dae2bf43e0486d513dfceb7ab319b7af
sca4cs <shannon.atkinson.ca@gmail.com>
2019-02-12 17:48:33 -0500

    set up portfolio with create react app

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import MainContent from "./MainContent";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MainContent/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-portfolio/src/MainContent.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class MainContent extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, my name is Shannon.</h1>
+            </div>
+        )
+    }
+}
+
+export default MainContent;

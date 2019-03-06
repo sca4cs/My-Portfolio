8379364aead346c7d72ef9a5d16834f335195009
sca4cs <shannon.atkinson.ca@gmail.com>
2019-03-06 13:51:00 -0500

    add circle backgrounds to social icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,9 +3,9 @@ import React from "react";
 const Socials = () => {
     return (
         <div className="socials">
-          <a href="https://github.com/sca4cs" target="_blank" rel="noopener noreferrer"><i className="fab fa-github fa-2x"></i></a>
-          <a href="https://www.linkedin.com/in/shannon-atkinson/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
-          <a href="mailto:shannon.atkinson.ca@gmail.com"><i className="fas fa-envelope fa-2x"></i></a>
+          <a href="https://github.com/sca4cs" target="_blank" rel="noopener noreferrer"><i className="fab fa-github fa-lg"></i></a>
+          <a href="https://www.linkedin.com/in/shannon-atkinson/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-lg"></i></a>
+          <a href="mailto:shannon.atkinson.ca@gmail.com"><i className="fas fa-envelope fa-lg"></i></a>
         </div>
         
     );

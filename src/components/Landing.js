903be95918163e12ebc5881098d2106fb32ff838
sca4cs <shannon.atkinson.ca@gmail.com>
2019-03-06 16:26:59 -0500

    auto scroll from landing to about section

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Socials from "./Contact";
 
-const Landing = () => {
+const Landing = (props) => {
     return (
         <div className="landing">
             <Socials/>
             <div className="intro">
             <h1 className="title">Shannon Atkinson</h1>
             <h2 className="subtitle">Full Stack Web Developer<br/>&amp;<br/>Software Engineer</h2>
+            <i onClick={props.scrollToMyRef} className="fas fa-arrow-circle-down fa-lg"></i>
             </div>
         </div>
     );
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -8,16 +8,29 @@ import Divider from '@material-ui/core/Divider';
 import './main.css';
 
 class MainContent extends Component {
+    constructor(props) {
+        super(props)
+        this.myRef = React.createRef()
+    }
+
+    scrollToMyRef = () => {
+        window.scrollTo({
+            top: this.myRef.current.offsetTop - 100,
+            behavior: 'smooth',
+          })
+    }
 
     render() {
         return (
             <div className="main-content">
-                <Landing/>
+                <Landing scrollToMyRef={this.scrollToMyRef}/>
+                <div ref={this.myRef}>
                 <Paper className="paper">
                     <About/>
                     <Divider />
                     <Projects/>
                 </Paper>
+                </div>
             </div>
         )
     }
